ade610bc26a3fe228e986edde40351e98e7a66c7
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2025-11-05 12:53:42 +0700

    feat: 1246 night

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -8,7 +8,7 @@ let json: any[] = [];
 const HARDENED = 0x80000000;
 const mnemonic = process.env.MNEMONIC!;
 const entropyHex = mnemonicToEntropy(mnemonic);
-
+let total: number = 0;
 
 for(let index = Number(process.env.ACCOUNT_INDEX_START); index < (Number(process.env.AMOUNT_ACCOUNT)+ Number(process.env.ACCOUNT_INDEX_START)); index ++) {
 
@@ -26,8 +26,11 @@ for(let index = Number(process.env.ACCOUNT_INDEX_START); index < (Number(process
     const address = await meshWallet.getChangeAddress();
    
     const {data} = await axios.get(`https://scavenger.prod.gd.midnighttge.io/statistics/${address}`)
-   
 
+
+
+   
+    total += Number(data?.local?.night_allocation) / 1_000_000;
     json.push({
         account_index: index,
         address: address,
@@ -37,4 +40,4 @@ for(let index = Number(process.env.ACCOUNT_INDEX_START); index < (Number(process
 
 
 fs.writeFileSync("./wallet.json", JSON.stringify(json, null, 4));
-console.log("✅ File wallets.json đã tạo thành công!");
+console.log(`✅ ${total} Night & File wallets.json đã tạo thành công !`);
